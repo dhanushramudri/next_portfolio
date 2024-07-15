b5ada3358aba9b2972ec67c7630591320e0672f8
dhanushramudri <ramudridhanush@gmail.com>
2024-07-15 23:01:42 +0530

    updated layout

--- app/page.tsx
+++ app/page.tsx
@@ -12,18 +12,16 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items=center flex-col mx-auto sm:px-10 px-5 overflow-clip ">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="text-green-400">
-          <FloatingNav navItems={navItems} />
-          <Hero />
-          <Grid />
-          <RecentProjects />
-          <Clients />
-          <Experience />
-          <Approach />
-          <Footer />
-        </h1>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
