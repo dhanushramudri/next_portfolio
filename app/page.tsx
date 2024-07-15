e49ad34d993f8a57480edcfa74471636498e377f
dhanushramudri <ramudridhanush@gmail.com>
2024-07-15 22:47:56 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -9,7 +12,7 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items=center flex-col overflow-hidden mx-auto sm:px-10 px-5 ">
+    <main className="relative bg-black-100 flex justify-center items=center flex-col mx-auto sm:px-10 px-5 overflow-clip ">
       <div className="max-w-7xl w-full">
         <h1 className="text-green-400">
           <FloatingNav navItems={navItems} />
@@ -17,6 +20,9 @@ export default function Home() {
           <Grid />
           <RecentProjects />
           <Clients />
+          <Experience />
+          <Approach />
+          <Footer />
         </h1>
       </div>
     </main>
